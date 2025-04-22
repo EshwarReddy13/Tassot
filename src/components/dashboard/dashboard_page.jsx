314eda5d750ac9803538505e29d7ab37a259badb
EshwarReddy13 <eshwarreddygadi@gmail.com>
2025-04-22 09:33:44 -0500

    Made a few changes to the dashboard page

--- src/components/dashboard/dashboard_page.jsx
+++ src/components/dashboard/dashboard_page.jsx
@@ -52,8 +52,8 @@ const DashboardPage = () => {
             ) : (
               <>
                 <motion.h1
-                  className="text-white text-2xl font-bold text-center"
-                  style={{ fontSize: 'clamp(1.8rem, 3vw, 2.3rem)' }}
+                  className="text-white text-3xl font-bold text-center"
+                  style={{ fontSize: 'clamp(1.8rem, 3vw, 2.5rem)' }}
                   initial={{ opacity: 0, y: -20 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5 }}
